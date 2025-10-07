c00dffa472780f746734a6d0542117d729f9eac5
Sprialljf <ljf628@bu.edu>
2025-10-06 22:45:15 -0400

    Delete src/main/java/zyjfassignment/boardgames directory

--- src/main/java/zyjfassignment/boardgames/core/GameEngine.java
+++
@@ -1,9 +0,0 @@
-
-package zyjfassignment.boardgames.core;
-
-import java.util.Scanner;
-
-public interface GameEngine {
-    String name();
-    void playLoop(Scanner in);
-}
--- src/main/java/zyjfassignment/boardgames/core/strategy/EdgeChoiceStrategy.java
+++
@@ -1,8 +0,0 @@
-
-package zyjfassignment.boardgames.core.strategy;
-
-import zyjfassignment.boardgames.dots.DotsBoard;
-
-public interface EdgeChoiceStrategy {
-    int choose(DotsBoard board, int selfId);
-}
